68544488d1de7987cb788b04f1c95021a63d55a2
maxixking66 <maxixking66@gamil.com>
2023-06-01 10:44:20 +0330

    add ApplicationContext

---
+++ src/main/java/ir/maktabsharif92/jpaexample/ApplicationContext.java
@@ -0,0 +1,37 @@
+package ir.maktabsharif92.jpaexample;
+
+import ir.maktabsharif92.jpaexample.repository.CustomerRepository;
+import ir.maktabsharif92.jpaexample.repository.impl.CustomerRepositoryImpl;
+import ir.maktabsharif92.jpaexample.service.CustomerService;
+import ir.maktabsharif92.jpaexample.service.CustomerServiceImpl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class ApplicationContext {
+
+    private static final EntityManager em =
+            Persistence.createEntityManagerFactory(
+                    "default"
+            ).createEntityManager();
+
+    private static CustomerRepository customerRepository;
+
+    private static CustomerService customerService;
+
+    public static CustomerRepository getCustomerRepository() {
+        if (customerRepository == null) {
+            customerRepository = new CustomerRepositoryImpl(em);
+        }
+        return customerRepository;
+    }
+
+    public static CustomerService getCustomerService() {
+        if (customerService == null) {
+            customerService = new CustomerServiceImpl(
+                    getCustomerRepository()
+            );
+        }
+        return customerService;
+    }
+}
